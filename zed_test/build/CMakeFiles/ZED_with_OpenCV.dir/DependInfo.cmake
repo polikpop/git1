e453c1378dbde5e1f123dc4f48c17e755b5b601e
xushuo <374933221@qq.com>
2020-12-25 15:09:56 +0800

    zed

---
+++ zed_test/build/CMakeFiles/ZED_with_OpenCV.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xushuo/CDe/zed_test/src/sd.cpp" "/home/xushuo/CDe/zed_test/build/CMakeFiles/ZED_with_OpenCV.dir/src/sd.o"
+  "/home/xushuo/CDe/zed_test/src/t1.cpp" "/home/xushuo/CDe/zed_test/build/CMakeFiles/ZED_with_OpenCV.dir/src/t1.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/local/zed/include"
+  "../include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ zed_test/build/CMakeFiles/ZED_with_OpenCV.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZED_with_OpenCV.dir/src/sd.o"
+  "CMakeFiles/ZED_with_OpenCV.dir/src/t1.o"
+  "ZED_with_OpenCV"
+  "ZED_with_OpenCV.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZED_with_OpenCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
